8efc2d3b4bfa1ec23475aeede88e1f3942e4bb8e
tom <tom@sapletta.com>
2021-06-13 15:56:06 +0200

    inject_script

---
+++ include/inject-script.js
@@ -0,0 +1,16 @@
+/**
+ * 	injectScript('test.js', () => {
+		console.log('test');
+		setInterval(test, 1000);
+	});
+
+ * @param source
+ * @param callback
+ */
+const inject_script = (source, callback) => {
+    const script = document.createElement('script');
+    script.src = source;
+    script.addEventListener('load', callback);
+    document.head.appendChild(script);
+};
+
